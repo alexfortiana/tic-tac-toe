28ebe08ccea4f0b07ea03625df024281f14de873
Alex Fortiana <alexfortiana@gmail.com>
2024-06-17 01:55:21 +0200

    States & game logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,102 @@
+import { useState } from "react";
+
 const TURNS = {
   X: 'X',
   O: 'O'
 }
 
-const board = Array(9).fill(null);
+const WINNER_COMBOS = [
+  [0, 1, 2], // top row
+  [3, 4, 5], // middle row
+  [6, 7, 8], // bottom row
+  [0, 3, 6], // left column
+  [1, 4, 7], // middle column
+  [2, 5, 8], // right column
+  [0, 4, 8], // diagonal
+  [2, 4, 6], // diagonal
+]
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`;
+
+  const handleClick = () => {
+    updateBoard(index);
+
+  }
+
+  return (
+    <div className={className} onClick={handleClick} >
+      {children}
+    </div>
+  )
+}
 
 function App() {
 
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X);
+  const [winner, setWinner] = useState(null);
+
+  const checkwinner = (boardToCheck) => {
+    for (const combo in WINNER_COMBOS) {
+      const [a, b, c] = combo;
+      if (boardToCheck[a] && boardToCheck[a] === boardToCheck[b] && boardToCheck[a] === boardToCheck[c]) {
+        return boardToCheck[a];
+      }
+    }
+    return null
+
+  }
+
+  const updateBoard = (index) => {
+    if(board[index] || winner) return
+
+    const newBoard = [...board];
+    newBoard[index] = turn;
+    setBoard(newBoard);
+
+
+
+    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
+    setTurn(newTurn);
+    
+
+    const newWinner = checkwinner(newBoard);
+
+    if(newWinner){
+      setWinner(newWinner);
+    }
+
+
+  }
 
   return (
     <main className="board">
       <h1>Tic Tac Toe</h1>
       <section
-      className="game">
+        className="game">
         {
-        board.map(( e, index) => {
-         return (
-          <div className="cell" key={index}>
-            <span className="cell__content">
-              {index}
-            </span>
-          </div>
-        )
-      }) 
-    }
+          board.map((e, index) => {
+            return (
+              <Square key={index} index={index} updateBoard={updateBoard}>
+                {e}
+              </Square>
+            )
+          })
+        }
+      </section>
+      <section className="turn">
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
       </section>
     </main>
-  ) 
-    
+  )
+
 }
 
 export default App
